812d72cefdcd9b649bbdd96dedd0069d0ff34938
pgrabarz <pgrabarz@pgs-soft.com>
2015-02-27 15:02:09 +0100

    sass docs

--- gulp-recipes/sass.js
+++ gulp-recipes/sass.js
@@ -12,20 +12,29 @@ module.exports = function ($, config, sources) {
 
     var sassSource = $.lazypipe()
         .pipe(sources.sass)
+        .pipe($.sourcemaps.init)
         .pipe(sassProcess);
 
     /**
      * @task sass
      * Compile all sass files and store in temp directory
+     * @config sources.sass sourcess for sass compilation
+     * @config paths.tmp temp folder location
      */
     $.gulp.task('sass', function () {
         var processCssPipe = $.utils.sequentialLazypipe($.utils.getPipes('devProcessCss'));
 
         return sassSource
             .pipe(processCssPipe)
+            .pipe($.sourcemaps.write)
             .pipe($.gulp.dest, config.paths.tmp)();
     });
 
+
+    /**
+     * @task watch:sass
+     * Runs sass watcher
+     */
     $.gulp.task('watch:sass', ['sass'], function () {
         $.utils.watchSource(sources.sass, function () {
             $.utils.runSubtasks('sass');
