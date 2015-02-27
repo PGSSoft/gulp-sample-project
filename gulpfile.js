a93eb5db1dc249c66a50f4c98f2633cd05a93028
pgrabarz <pgrabarz@pgs-soft.com>
2015-02-27 15:36:54 +0100

    extended config

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
-var config = require('require-dir')('gulp-config');
+var _ = require('lodash');
+
+var config = _.extend(require('require-dir')('gulp-config'), {
+    useHistoryApi: false
+});
 
 var $ = require('gulp-recipe-loader')(require('gulp'), config);
 
